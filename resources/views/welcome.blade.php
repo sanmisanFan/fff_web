a45055d833c68e9fc7a87e148ddd7478823cf765
Fan Lei <leifan833@msn.com>
2016-06-01 14:13:38 +0800

    change welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <p>dance with me...</p>
             </div>
         </div>
     </body>
